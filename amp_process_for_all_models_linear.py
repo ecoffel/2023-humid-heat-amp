ea3ea3cec8741a2573888c1e9aa9547ea802b7b0
edcoffel <edcoffel@noreply.syr.edu>
2024-03-22 16:20:59 -0400

    su 2024

--- amp_process_for_all_models_linear.py
+++ amp_process_for_all_models_linear.py
@@ -8,9 +8,9 @@ cmip6_models = ['access-cm2',
                 'mpi-esm1-2-hr', 'mpi-esm1-2-lr',
                 'mri-esm2-0', 'noresm2-lm', 'taiesm1']
 
-for model in range(len(cmip6_models)):
+for model in range(1, len(cmip6_models)):
     
     print(f'running {model}')
-    os.system(f'ipython amp_calc_cmip6_data.py {model}')
+    os.system(f'ipython amp_calc_cmip6_pr_on_txx.py {model}')
 
     
