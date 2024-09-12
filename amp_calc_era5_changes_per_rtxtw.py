6abe877db5c3f7309f3e513879f2c0a5b80bbbd9
edcoffel <edcoffel@noreply.syr.edu>
2024-09-12 14:55:25 -0400

    su 2024

--- amp_calc_era5_changes_per_rtxtw.py
+++ amp_calc_era5_changes_per_rtxtw.py
@@ -68,12 +68,14 @@ era5_n_days_above_tx_90_per_rtet = np.full([era5_huss_on_txx_full.latitude.size,
 era5_n_days_above_tx_90_per_rtet_r2 = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 
 era5_n_days_above_tx_99_per_rtet = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
+era5_n_days_above_tx_99_per_rtet_ci = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size, 2], np.nan)
 era5_n_days_above_tx_99_per_rtet_r2 = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 
 era5_n_days_above_tw_90_per_rtet = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 era5_n_days_above_tw_90_per_rtet_r2 = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 
 era5_n_days_above_tw_99_per_rtet = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
+era5_n_days_above_tw_99_per_rtet_ci = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size, 2], np.nan)
 era5_n_days_above_tw_99_per_rtet_r2 = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 
 era5_tw_on_txx_per_rtet = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
@@ -98,6 +100,7 @@ era5_tx_on_txx_per_rtet = np.full([era5_huss_on_txx_full.latitude.size, era5_hus
 era5_tx_on_txx_per_rtet_int = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 era5_tx_on_txx_per_rtet_r2 = np.full([era5_huss_on_txx_full.latitude.size, era5_huss_on_txx_full.longitude.size], np.nan)
 
+
 for xlat in range(era5_huss_on_txx_full.latitude.size):
     if xlat%25==0 and xlat > 10:
         print(xlat)
@@ -105,20 +108,22 @@ for xlat in range(era5_huss_on_txx_full.latitude.size):
     for ylon in range(era5_huss_on_txx_full.longitude.size):
         
 
-#         v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
-#         v2 = era5_n_days_above_tw99_full.tw[:, xlat, ylon]
+        v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
+        v2 = era5_n_days_above_tw99_full.tw[:, xlat, ylon]
         
-#         nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
+        nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
         
-#         if nn.size > 20:
-#             v1 = signal.detrend(v1[nn])
-#             v2 = signal.detrend(v2[nn])
+        if nn.size > 20:
+            v1 = signal.detrend(v1[nn])
+            v2 = signal.detrend(v2[nn])
 
-#             X = sm.add_constant(v1[nn])
-#             mdl = sm.OLS(v2[nn], X).fit()
+            X = sm.add_constant(v1[nn])
+            mdl = sm.OLS(v2[nn], X).fit()
 
-#             era5_n_days_above_tw_99_per_rtet[xlat, ylon] = mdl.params[1]
-#             era5_n_days_above_tw_99_per_rtet_r2[xlat, ylon] = mdl.rsquared
+            era5_n_days_above_tw_99_per_rtet[xlat, ylon] = mdl.params[1]
+            era5_n_days_above_tw_99_per_rtet_ci[xlat, ylon, 0] = mdl.conf_int(alpha=.05)[1,0]
+            era5_n_days_above_tw_99_per_rtet_ci[xlat, ylon, 1] = mdl.conf_int(alpha=.05)[1,1]
+            era5_n_days_above_tw_99_per_rtet_r2[xlat, ylon] = mdl.rsquared
         
 #         v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
 #         v2 = era5_n_days_above_tw90_full.tw[:, xlat, ylon]
@@ -136,20 +141,22 @@ for xlat in range(era5_huss_on_txx_full.latitude.size):
 #             era5_n_days_above_tw_90_per_rtet_r2[xlat, ylon] = mdl.rsquared
             
         
-#         v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
-#         v2 = era5_n_days_above_tx99_full.mx2t[:, xlat, ylon]
+        v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
+        v2 = era5_n_days_above_tx99_full.mx2t[:, xlat, ylon]
         
-#         nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
+        nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
         
-#         if nn.size > 20:
-#             v1 = signal.detrend(v1[nn])
-#             v2 = signal.detrend(v2[nn])
+        if nn.size > 20:
+            v1 = signal.detrend(v1[nn])
+            v2 = signal.detrend(v2[nn])
 
-#             X = sm.add_constant(v1[nn])
-#             mdl = sm.OLS(v2[nn], X).fit()
+            X = sm.add_constant(v1[nn])
+            mdl = sm.OLS(v2[nn], X).fit()
 
-#             era5_n_days_above_tx_99_per_rtet[xlat, ylon] = mdl.params[1]
-#             era5_n_days_above_tx_99_per_rtet_r2[xlat, ylon] = mdl.rsquared
+            era5_n_days_above_tx_99_per_rtet[xlat, ylon] = mdl.params[1]
+            era5_n_days_above_tx_99_per_rtet_ci[xlat, ylon, 0] = mdl.conf_int(alpha=.05)[1,0]
+            era5_n_days_above_tx_99_per_rtet_ci[xlat, ylon, 1] = mdl.conf_int(alpha=.05)[1,1]
+            era5_n_days_above_tx_99_per_rtet_r2[xlat, ylon] = mdl.rsquared
         
 #         v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
 #         v2 = era5_n_days_above_tx90_full.mx2t[:, xlat, ylon]
@@ -168,35 +175,35 @@ for xlat in range(era5_huss_on_txx_full.latitude.size):
             
             
         
-        v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
-        v2 = era5_vpd_daily_mean_on_tww_full.vpd_daily_mean_on_tww[:, xlat, ylon]
+#         v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
+#         v2 = era5_vpd_daily_mean_on_tww_full.vpd_daily_mean_on_tww[:, xlat, ylon]
         
-        nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
+#         nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
         
-        if nn.size > 20:
-            v1 = signal.detrend(v1[nn])
-            v2 = signal.detrend(v2[nn])
+#         if nn.size > 20:
+#             v1 = signal.detrend(v1[nn])
+#             v2 = signal.detrend(v2[nn])
 
-            X = sm.add_constant(v1[nn])
-            mdl = sm.OLS(v2[nn], X).fit()
+#             X = sm.add_constant(v1[nn])
+#             mdl = sm.OLS(v2[nn], X).fit()
 
-            era5_vpd_daily_mean_on_tww_per_rtet[xlat, ylon] = mdl.params[1]
-            era5_vpd_daily_mean_on_tww_per_rtet_r2[xlat, ylon] = mdl.rsquared
+#             era5_vpd_daily_mean_on_tww_per_rtet[xlat, ylon] = mdl.params[1]
+#             era5_vpd_daily_mean_on_tww_per_rtet_r2[xlat, ylon] = mdl.rsquared
         
-        v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
-        v2 = era5_vpd_daily_mean_on_txx_full.vpd_daily_mean_on_txx[:, xlat, ylon]
+#         v1 = era5_tx_tw_corr_full.tx_tw_corr[:,xlat,ylon]
+#         v2 = era5_vpd_daily_mean_on_txx_full.vpd_daily_mean_on_txx[:, xlat, ylon]
         
-        nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
+#         nn = np.where((~np.isnan(v1)) & (~np.isnan(v2)))[0]
         
-        if nn.size > 20:
-            v1 = signal.detrend(v1[nn])
-            v2 = signal.detrend(v2[nn])
+#         if nn.size > 20:
+#             v1 = signal.detrend(v1[nn])
+#             v2 = signal.detrend(v2[nn])
 
-            X = sm.add_constant(v1[nn])
-            mdl = sm.OLS(v2[nn], X).fit()
+#             X = sm.add_constant(v1[nn])
+#             mdl = sm.OLS(v2[nn], X).fit()
 
-            era5_vpd_daily_mean_on_txx_per_rtet[xlat, ylon] = mdl.params[1]
-            era5_vpd_daily_mean_on_txx_per_rtet_r2[xlat, ylon] = mdl.rsquared
+#             era5_vpd_daily_mean_on_txx_per_rtet[xlat, ylon] = mdl.params[1]
+#             era5_vpd_daily_mean_on_txx_per_rtet_r2[xlat, ylon] = mdl.rsquared
         
         
         
@@ -342,10 +349,12 @@ for xlat in range(era5_huss_on_txx_full.latitude.size):
 
 
 
-# with open('era5_n_days_above_tw_99_per_rtet.dat', 'wb') as f:
-#     pickle.dump(era5_n_days_above_tw_99_per_rtet, f)
-# with open('era5_n_days_above_tw_99_per_rtet_r2.dat', 'wb') as f:
-#     pickle.dump(era5_n_days_above_tw_99_per_rtet_r2, f)  
+with open('era5_n_days_above_tw_99_per_rtet.dat', 'wb') as f:
+    pickle.dump(era5_n_days_above_tw_99_per_rtet, f)
+with open('era5_n_days_above_tw_99_per_rtet_ci.dat', 'wb') as f:
+    pickle.dump(era5_n_days_above_tw_99_per_rtet_ci, f)
+with open('era5_n_days_above_tw_99_per_rtet_r2.dat', 'wb') as f:
+    pickle.dump(era5_n_days_above_tw_99_per_rtet_r2, f)  
 
 # with open('era5_n_days_above_tw_90_per_rtet.dat', 'wb') as f:
 #     pickle.dump(era5_n_days_above_tw_90_per_rtet, f)
@@ -353,25 +362,27 @@ for xlat in range(era5_huss_on_txx_full.latitude.size):
 #     pickle.dump(era5_n_days_above_tw_90_per_rtet_r2, f)    
 
 
-# with open('era5_n_days_above_tx_99_per_rtet.dat', 'wb') as f:
-#     pickle.dump(era5_n_days_above_tx_99_per_rtet, f)
-# with open('era5_n_days_above_tx_99_per_rtet_r2.dat', 'wb') as f:
-#     pickle.dump(era5_n_days_above_tx_99_per_rtet_r2, f)  
+with open('era5_n_days_above_tx_99_per_rtet.dat', 'wb') as f:
+    pickle.dump(era5_n_days_above_tx_99_per_rtet, f)
+with open('era5_n_days_above_tx_99_per_rtet_ci.dat', 'wb') as f:
+    pickle.dump(era5_n_days_above_tx_99_per_rtet_ci, f)
+with open('era5_n_days_above_tx_99_per_rtet_r2.dat', 'wb') as f:
+    pickle.dump(era5_n_days_above_tx_99_per_rtet_r2, f)  
 
 # with open('era5_n_days_above_tx_90_per_rtet.dat', 'wb') as f:
 #     pickle.dump(era5_n_days_above_tx_90_per_rtet, f)
 # with open('era5_n_days_above_tx_90_per_rtet_r2.dat', 'wb') as f:
 #     pickle.dump(era5_n_days_above_tx_90_per_rtet_r2, f)    
 
-with open('era5_vpd_daily_mean_on_tww_per_rtet.dat', 'wb') as f:
-    pickle.dump(era5_vpd_daily_mean_on_tww_per_rtet, f)
-with open('era5_vpd_daily_mean_on_tww_per_rtet_r2.dat', 'wb') as f:
-    pickle.dump(era5_vpd_daily_mean_on_tww_per_rtet_r2, f)    
+# with open('era5_vpd_daily_mean_on_tww_per_rtet.dat', 'wb') as f:
+#     pickle.dump(era5_vpd_daily_mean_on_tww_per_rtet, f)
+# with open('era5_vpd_daily_mean_on_tww_per_rtet_r2.dat', 'wb') as f:
+#     pickle.dump(era5_vpd_daily_mean_on_tww_per_rtet_r2, f)    
 
-with open('era5_vpd_daily_mean_on_txx_per_rtet.dat', 'wb') as f:
-    pickle.dump(era5_vpd_daily_mean_on_txx_per_rtet, f)
-with open('era5_vpd_daily_mean_on_txx_per_rtet_r2.dat', 'wb') as f:
-    pickle.dump(era5_vpd_daily_mean_on_txx_per_rtet_r2, f)    
+# with open('era5_vpd_daily_mean_on_txx_per_rtet.dat', 'wb') as f:
+#     pickle.dump(era5_vpd_daily_mean_on_txx_per_rtet, f)
+# with open('era5_vpd_daily_mean_on_txx_per_rtet_r2.dat', 'wb') as f:
+#     pickle.dump(era5_vpd_daily_mean_on_txx_per_rtet_r2, f)    
 
 
 # with open('era5_huss_on_txx_per_rtet.dat', 'wb') as f:
